49c5aa9adc6d47ee6b265c1755838caf1589666b
Yuta Ohira <alesion90@gmail.com>
2021-12-18 19:50:56 +0900

    :art: xxxページの状態を定義

---
+++ lib/ui/xxx/xxx_state.dart
@@ -0,0 +1,10 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'xxx_state.freezed.dart';
+
+@freezed
+class XXXState with _$XXXState {
+  const factory XXXState({
+    @Default("") int hoge,
+  }) = _XXXState;
+}
---
+++ lib/ui/xxx/xxx_state.freezed.dart
@@ -0,0 +1,138 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'xxx_state.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$XXXStateTearOff {
+  const _$XXXStateTearOff();
+
+  _XXXState call({int hoge = ""}) {
+    return _XXXState(
+      hoge: hoge,
+    );
+  }
+}
+
+/// @nodoc
+const $XXXState = _$XXXStateTearOff();
+
+/// @nodoc
+mixin _$XXXState {
+  int get hoge => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $XXXStateCopyWith<XXXState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $XXXStateCopyWith<$Res> {
+  factory $XXXStateCopyWith(XXXState value, $Res Function(XXXState) then) =
+      _$XXXStateCopyWithImpl<$Res>;
+  $Res call({int hoge});
+}
+
+/// @nodoc
+class _$XXXStateCopyWithImpl<$Res> implements $XXXStateCopyWith<$Res> {
+  _$XXXStateCopyWithImpl(this._value, this._then);
+
+  final XXXState _value;
+  // ignore: unused_field
+  final $Res Function(XXXState) _then;
+
+  @override
+  $Res call({
+    Object? hoge = freezed,
+  }) {
+    return _then(_value.copyWith(
+      hoge: hoge == freezed
+          ? _value.hoge
+          : hoge // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$XXXStateCopyWith<$Res> implements $XXXStateCopyWith<$Res> {
+  factory _$XXXStateCopyWith(_XXXState value, $Res Function(_XXXState) then) =
+      __$XXXStateCopyWithImpl<$Res>;
+  @override
+  $Res call({int hoge});
+}
+
+/// @nodoc
+class __$XXXStateCopyWithImpl<$Res> extends _$XXXStateCopyWithImpl<$Res>
+    implements _$XXXStateCopyWith<$Res> {
+  __$XXXStateCopyWithImpl(_XXXState _value, $Res Function(_XXXState) _then)
+      : super(_value, (v) => _then(v as _XXXState));
+
+  @override
+  _XXXState get _value => super._value as _XXXState;
+
+  @override
+  $Res call({
+    Object? hoge = freezed,
+  }) {
+    return _then(_XXXState(
+      hoge: hoge == freezed
+          ? _value.hoge
+          : hoge // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_XXXState implements _XXXState {
+  const _$_XXXState({this.hoge = ""});
+
+  @JsonKey()
+  @override
+  final int hoge;
+
+  @override
+  String toString() {
+    return 'XXXState(hoge: $hoge)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _XXXState &&
+            const DeepCollectionEquality().equals(other.hoge, hoge));
+  }
+
+  @override
+  int get hashCode =>
+      Object.hash(runtimeType, const DeepCollectionEquality().hash(hoge));
+
+  @JsonKey(ignore: true)
+  @override
+  _$XXXStateCopyWith<_XXXState> get copyWith =>
+      __$XXXStateCopyWithImpl<_XXXState>(this, _$identity);
+}
+
+abstract class _XXXState implements XXXState {
+  const factory _XXXState({int hoge}) = _$_XXXState;
+
+  @override
+  int get hoge;
+  @override
+  @JsonKey(ignore: true)
+  _$XXXStateCopyWith<_XXXState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
