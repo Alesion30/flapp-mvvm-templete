3e73ef122721962e4d7861ec4d48e43af7826ee9
Yuta Ohira <alesion90@gmail.com>
2021-12-18 18:54:38 +0900

    :art: AssetGenImageに対応

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flapp/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
 
@@ -32,10 +33,7 @@ class MyHomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset(
-              'assets/img/flutter-icon.png',
-              width: 200,
-            ),
+            Assets.img.flutterIcon.image(width: 200),
             Text(
               L10n.of(context)!.hello,
               style: Theme.of(context).textTheme.headline4,
