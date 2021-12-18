bd614a7b345b4ff057b85ba66f9b64339af8f000
Yuta Ohira <alesion90@gmail.com>
2021-12-18 20:55:19 +0900

    :zap: AppRouterをメモ化

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,15 @@
 import 'package:flapp/ui/routes/app_route.gr.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends HookConsumerWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final appRouter = AppRouter();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final appRouter = useMemoized(() => AppRouter());
 
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
