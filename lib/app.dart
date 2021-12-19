f16004d8ed1459641322a8fab965eaa998d133b4
Yuta Ohira <alesion90@gmail.com>
2021-12-19 15:23:16 +0900

    :zap: Sizerを導入

--- lib/app.dart
+++ lib/app.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:sizer/sizer.dart';
 
 class MyApp extends HookConsumerWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -14,15 +15,17 @@ class MyApp extends HookConsumerWidget {
     final themeMode = ref.watch(appThemeModeProvider);
     final appRouter = useMemoized(() => AppRouter());
 
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      theme: theme.data,
-      darkTheme: AppTheme.dark().data,
-      themeMode: themeMode,
-      localizationsDelegates: L10n.localizationsDelegates,
-      supportedLocales: L10n.supportedLocales,
-      routeInformationParser: appRouter.defaultRouteParser(),
-      routerDelegate: appRouter.delegate(),
+    return Sizer(
+      builder: (context, orientation, deviceType) => MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        theme: theme.data,
+        darkTheme: AppTheme.dark().data,
+        themeMode: themeMode,
+        localizationsDelegates: L10n.localizationsDelegates,
+        supportedLocales: L10n.supportedLocales,
+        routeInformationParser: appRouter.defaultRouteParser(),
+        routerDelegate: appRouter.delegate(),
+      ),
     );
   }
 }
