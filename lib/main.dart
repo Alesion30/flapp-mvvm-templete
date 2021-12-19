bd6bca4a83ab9ff9175f56220bc44f1a0a17822e
Yuta Ohira <alesion90@gmail.com>
2021-12-19 15:36:35 +0900

    :loud_sound: Fimerの設定・エラーログを追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,24 @@
+import 'dart:async';
+
+import 'package:fimber/fimber.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+
 import 'app.dart';
 
 void main() {
-  runApp(const ProviderScope(child: MyApp()));
+  // Fimber
+  if (!kReleaseMode) {
+    Fimber.plantTree(DebugTree());
+  } else {
+    debugPrint = (message, {wrapWidth}) {};
+  }
+
+  runZonedGuarded(
+    () => runApp(const ProviderScope(child: MyApp())),
+    (error, stackTrace) {
+      Fimber.e(error.toString());
+    },
+  );
 }
