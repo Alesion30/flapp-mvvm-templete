f072f7c8e4f1fe1bfdb971b1f322e6508057af37
Yuta Ohira <alesion90@gmail.com>
2021-12-19 16:24:02 +0900

    :sparkles: dev環境時はtoastを表示するように実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,30 @@
 import 'dart:async';
 
+import 'package:enum_to_string/enum_to_string.dart';
 import 'package:fimber/fimber.dart';
+import 'package:flapp/foundation/constants.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   // Fimber
   if (!kReleaseMode) {
     Fimber.plantTree(DebugTree());
   } else {
     debugPrint = (message, {wrapWidth}) {};
+  }
+
+  // Display Toast (Only Dev)
+  if (Constants.flavor == Flavor.dev) {
+    Fluttertoast.showToast(
+      msg: "flavor: ${EnumToString.convertToString(Constants.flavor)}",
+    );
   }
 
   runZonedGuarded(
